552b1f2eff2bca5a33045cdda1b0083c2df2cdb7
danyun <danyun@qiyi.com>
2017-03-27 01:48:21 +0800

    update shape exc file

---
+++ app/src/main/java/com/huangshan/demo/exercise/ExcShapeActivity.kt
@@ -0,0 +1,14 @@
+package com.huangshan.demo.exercise
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.huangshan.demo.R
+
+class ExcShapeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exc_shape)
+    }
+}
